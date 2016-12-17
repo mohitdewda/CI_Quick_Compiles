41a80d41b66ef21e8ee17e4167af84e11b84d478
Himanshu Shrivastava <himanshu31shr@gmail.com>
2016-12-17 18:01:22 +0530

    Commit more function

--- most_used_queries.php
+++ most_used_queries.php
@@ -5,8 +5,12 @@
 	$query = $this->db->get();
 	$data = $query->result();														// returns data in array of objects form
 	
-	/* Get all data from a table */
+	/* all DATA */
 	$query = $this->db->get('mytable')->result();									// returns data in array of objects form
 	
-	/* Get all data from a table with limits */
+	/* all DATA with LIMIT */
 	$query = $this->db->get('mytable', $lower_lim, $upper_lim )->result();			// returns data in array of objects form
+	
+	/* all DATA with WHERE, LIMIT */
+	$query = $this->db->get_where('mytable', array('id' => $id), $limit, $offset)	// returns data in array of objects form
+					->result()
