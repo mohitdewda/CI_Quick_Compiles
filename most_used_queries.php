ac295f40fc26f2cedd077c5399edf7fc951c5268
Himanshu Shrivastava <himanshu31shr@gmail.com>
2016-12-17 17:33:02 +0530

    Added Select queries

--- most_used_queries.php
+++ most_used_queries.php
@@ -1,22 +1,12 @@
 <?php
-/* Select Query, Using result(), @returns Std Object */
-
+	/* Select Query, Using result(), @returns Std Object */
 	$this->db->from('');
 	$this->db->where('', $identifier);
 	$query = $this->db->get();
-	$data = $query->result();
+	$data = $query->result();														// returns data in array of objects form
 	
-	/*
-	Data format
-	$data = array(
-		[0]=> stdObject(
-			''->..,
-			''->..,
-		),
-		[1]=> stdObject(
-			''->..,
-			''->..,
-		),		
-		..
-	)
-	*/
+	/* Get all data from a table */
+	$query = $this->db->get('mytable')->result();									// returns data in array of objects form
+	
+	/* Get all data from a table with limits */
+	$query = $this->db->get('mytable', $lower_lim, $upper_lim )->result();			// returns data in array of objects form
