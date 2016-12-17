15998967775137977f20c5946ca7934960edc5ed
Himanshu Shrivastava <himanshu31shr@gmail.com>
2016-12-17 18:02:21 +0530

    Changes

--- most_used_queries.php
+++ most_used_queries.php
@@ -6,11 +6,11 @@
 	$data = $query->result();														// returns data in array of objects form
 	
 	/* all DATA */
-	$query = $this->db->get('mytable')->result();									// returns data in array of objects form
+	$query = $this->db->get('mytable')->result();						// returns data in array of objects form
 	
 	/* all DATA with LIMIT */
 	$query = $this->db->get('mytable', $lower_lim, $upper_lim )->result();			// returns data in array of objects form
 	
 	/* all DATA with WHERE, LIMIT */
-	$query = $this->db->get_where('mytable', array('id' => $id), $limit, $offset)	// returns data in array of objects form
+	$query = $this->db->get_where('mytable', array('id' => $id), $limit, $offset)		// returns data in array of objects form
 					->result()
